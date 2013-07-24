d3f4c9d6516ff83f7d7dc855fcc0ed8381d59205
mmccurdy07 <mmccurdy@gmail.com>
2013-07-23 21:59:39 -0700

    Add more languages that the Freebase metadata now supports.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -36,7 +36,15 @@ class PlexMovieAgent(Agent.Movies):
   name = 'Freebase'
   languages = [Locale.Language.English, Locale.Language.Swedish, Locale.Language.French, 
                Locale.Language.Spanish, Locale.Language.Dutch, Locale.Language.German, 
-               Locale.Language.Italian, Locale.Language.Danish]
+               Locale.Language.Italian, Locale.Language.Danish,Locale.Language.Arabic, 
+               Locale.Language.Catalan, Locale.Language.Chinese, Locale.Language.Czech,
+               Locale.Language.Estonian, Locale.Language.Finnish, Locale.Language.Greek,
+               Locale.Language.Hebrew, Locale.Language.Hindi, Locale.Language.Hungarian,
+               Locale.Language.Indonesian, Locale.Language.Japanese, Locale.Language.Korean,
+               Locale.Language.Latvian, Locale.Language.Norwegian, Locale.Language.Persian,
+               Locale.Language.Polish, Locale.Language.Portuguese, Locale.Language.Romanian,
+               Locale.Language.Russian, Locale.Language.Slovak, Locale.Language.Thai,
+               Locale.Language.Turkish, Locale.Language.Ukranian, Locale.Language.Vietnamese]
 
   def identifierize(self, string):
       string = re.sub( r"\s+", " ", string.strip())
@@ -82,7 +90,7 @@ class PlexMovieAgent(Agent.Movies):
 
 
   def getPlexMovieResults(self, media, matches, search_type='hash', plex_hash=''):
-    if search_type is 'hash':
+    if search_type is 'hash' and plex_hash is not None:
       url = '%s/%s/hash/%s/%s.xml' % (PLEXMOVIE_URL, PLEXMOVIE_BASE, plex_hash[0:2], plex_hash)
     else:
       titleyear_guid = self.titleyear_guid(media.name,media.year)
